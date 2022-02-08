51ea14f8a4fbd39538fd212b7f3959b21c5eb0da
AniketGaudgaul <aniketgaudgaul@gmail.com>
2022-02-08 10:29:52 +0530

    Customer Care Page done

--- kronos/shop/views.py
+++ kronos/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Contact
 # Create your views here.
 
 def index(request):
@@ -12,4 +13,13 @@ def customizations(request):
     return render(request, 'customizations.html')
 
 def customer(request):
-    return render(request, 'customer.html')
+    thank = False
+    if request.method=="POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        query = request.POST.get('query', '')
+        contact = Contact(name=name, email=email, query=query)
+        contact.save()
+        thank = True
+
+    return render(request, 'customer.html', {'thank':thank})
