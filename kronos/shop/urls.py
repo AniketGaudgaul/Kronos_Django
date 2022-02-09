69124c7409c93ac32ff575f8f020417ea88a4e05
AniketGaudgaul <aniketgaudgaul@gmail.com>
2022-02-09 14:00:14 +0530

    Authentication system Upstarted

--- kronos/shop/urls.py
+++ kronos/shop/urls.py
@@ -1,3 +1,4 @@
+from email import message
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -7,4 +8,7 @@ path('', views.index, name="ShopHome"),
 path('watches/', views.watches, name="Watches"),
 path('customizations/', views.customizations, name="Customizations"),
 path('customer/', views.customer, name="Customer"),
+path('signUp/', views.signUp, name="signUp"),
+path('login/', views.loginUser, name="login"),
+path('logout/', views.logoutUser, name="logout"),
 ]
--- kronos/shop/views.py
+++ kronos/shop/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . models import Contact, Product
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 
 def index(request):
@@ -8,8 +12,26 @@ def index(request):
     return render(request, 'index.html')
 
 def watches(request):
-    allProds = Product.objects.all()
-    print(allProds)
+    allProds = []
+    catprods = Product.objects.values('category')
+    # print(catprods)
+    cats = {item['category'] for item in catprods}
+    # print(cats)
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        print(prod)
+        n = len(prod)
+        print(n)
+        if(n%3==0):
+            outer = int(n/3)
+            print(outer)
+        else:
+            outer = n//3 + 1    
+        allProds.append([cat, range(outer),range(n), prod])
+
+
+    # allProds = Product.objects.all()
+    # print(allProds)
     params = {'allProds':allProds}
 
     return render(request, 'watches.html', params)
@@ -28,3 +50,42 @@ def customer(request):
         thank = True
 
     return render(request, 'customer.html', {'thank':thank})
+
+def signUp(request):
+    message = False
+    if request.method=="POST":
+        first_name = request.POST.get('first_name', '')
+        last_name = request.POST.get('last_name', '')
+        username = request.POST.get('username', '')
+        email = request.POST.get('email', '')
+        password = request.POST.get('password', '')
+        user = User.objects.create_user(username=username, password= password, email= email, first_name=first_name, last_name=last_name)
+        messages.success(request, 'Your Kronos account has been successfully created!')
+        user.save()
+    return redirect('ShopHome')
+
+
+def loginUser(request):
+    if request.method=="POST":
+        username = request.POST.get('login_username')
+        password = request.POST.get('login_password')
+
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            login(request,user)
+            messages.success(request, "Successfully logged in")
+            print("success login")
+            return redirect('ShopHome')
+        else:
+            messages.error(request, "Error login")
+            print("Error login")
+            return redirect('ShopHome')
+
+
+def logoutUser(request):
+
+    logout(request)
+    print("Logged out")
+    return redirect('ShopHome')
+
